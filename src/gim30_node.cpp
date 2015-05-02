f0e47b7d02b1a5d3db764b2dd2f5088f5b5cb0a6
Ryodo Tanaka <GRoadPG@gmail.com>
2015-05-03 04:27:39 +0900

    first commit nodelet
    nodeletを使用する初めてのコミット。
    しかし、nodeletにするかどうかが重要ではないことが判明したため、放置。

--- src/gim30_node.cpp
+++ src/gim30_node.cpp
@@ -1,115 +1,197 @@
 #include <ros/ros.h>
-#include <iostream>
+#include <nodelet/nodelet.h>
 #include <sensor_msgs/PointCloud.h>
-#include <sensor_msgs/PointCloud2.h> 
-#include <sensor_msgs/point_cloud_conversion.h>
-#include <cmath>
+#include <sensor_msgs/PointCloud2.h>
+#include <sensor_msgs/LaserScan.h>
+#include <diagnostic_msgs/DiagnosticStatus.h>
 
-#include "interface.h"
-
-std::string Gim30portname;
-std::string LRFportname;
-
-cirkit::Gim30Interface *Gim30;
+#include <iostream>
+#include <termios.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
 
 using namespace std;
 
-int main(int argc, char** argv){
-  ros::init(argc, argv, "gim30_3d");
-  ROS_INFO("Gim30 3D for ROS");
+// This class is getting the 3d data from gim30 + hokuyo2d
+// To get hokuyo2d data(2D data), we use urg libraries.
+// So, you have to install that libraries. 
+// For more info, Go "http://sourceforge.net/p/urgnetwork/wiki/top_jp/" 
+// About gim30, Go "http://ci.nii.ac.jp/els/110007503932.pdf?id=ART0009334101&type=pdf&lang=jp&host=cinii&order_no=&ppv_type=0&lang_sw=&no=1430472704&cp="
+namespace Gim30
+{
+  class Control{
+  public:
+    Control();
+    ~Control();
+    
+    float old_angle;
+    float new_angle;
 
-  ros::NodeHandle n;
-  ros::Publisher cloud_pub = n.advertise<sensor_msgs::PointCloud>("/gim30_3d", 50);
+  private:
+    void SetGim30();
+    void CloseGim30();
+    void StartGim30();
+    void StopGim30();
 
-  n.param<std::string>("gim30/Gim30portname",Gim30portname,"/dev/serial/by-id/usb-Prolific_Technology_Inc._USB-Serial_Controller-if00-port0");
-  n.param<std::string>("gim30/LRFportname", LRFportname, "192.168.0.10");
+    void callback(const diagnostic_msgs::DiagnosticStatus::ConstPtr& diag_msg);
+    ros::Subscriber diag_msg_sub;
 
-  Gim30 = new cirkit::Gim30Interface(Gim30portname, LRFportname);
-
-  sensor_msgs::PointCloud GIM3Ddata;
-  // Gim30 3D points const----------------------------
-  // GIM3Ddata.header.stamp = ros::Time::now();
-  GIM3Ddata.header.frame_id = "/base_link";
-  // Add intensity data channel
-  GIM3Ddata.channels.resize(1);
-  GIM3Ddata.channels[0].name = "intensities";
-
-  //  GIM3Ddata.points.resize(1081);
-
-  GIM3Ddata.points.resize(Gim30->LRFdata.ranges.size());
-  GIM3Ddata.channels[0].values.resize(Gim30->LRFdata.intensities.size());
-
-  sensor_msgs::PointCloud2 GIM3Ddata_pc2;
-  ros::Publisher cloud_pub_pc2 = n.advertise<sensor_msgs::PointCloud2>("/gim30_3d_pc2", 50);
-
-  double alpha, beta;
-  double tmp;
-  //--------------------------------------------------
-
-  ros::Rate r(40.0);
-
-  while(n.ok()){
-
-    switch(Gim30->Get3DData(true)){
-    case true :
-
-      for(double i=0; i<=Gim30->get_steps; i++){
-	if(Gim30->LRFdata.ranges[i] == 0){
-	  GIM3Ddata.points[i].x = 0;
-	  GIM3Ddata.points[i].y = 0;
-	  GIM3Ddata.points[i].z = 0;
-	}
-	else{
-	  tmp = Gim30->GIMangle_new-Gim30->GIMangle_old;
-	  if(tmp > 180)
-	    tmp = 360 - tmp;
-	  tmp = (LRF_END_DEG-LRF_START_DEG)*tmp*i/(360.0*Gim30->get_steps) + Gim30->GIMangle_old + (180.0+LRF_START_DEG)/360.0*tmp;
-	  alpha = atan(0.57734*sin(tmp*M_PI/180.0));
-	  beta = -atan(0.57734*cos(tmp*M_PI/180.0)*cos(alpha));
-	  tmp = (Gim30->LRFdata.angle_max - Gim30->LRFdata.angle_min)*i/Gim30->get_steps + Gim30->LRFdata.angle_min;    
-	  GIM3Ddata.points[i].x = Gim30->LRFdata.ranges[i]*cos(tmp)*cos(beta) + 0.039*sin(beta);
-	  GIM3Ddata.points[i].y = Gim30->LRFdata.ranges[i]*cos(tmp)*sin(alpha)*sin(beta) + Gim30->LRFdata.ranges[i]*sin(tmp)*cos(alpha) - 0.039*sin(alpha)*cos(beta);
-	  GIM3Ddata.points[i].z = -Gim30->LRFdata.ranges[i]*cos(tmp)*cos(alpha)*sin(beta) + Gim30->LRFdata.ranges[i]*sin(tmp)*sin(alpha) + 0.039*cos(alpha)*cos(beta);
-
-	  GIM3Ddata.channels[0].values[i] = Gim30->LRFdata.intensities[i];
-	}
-      } 
-
-      break;
-    default :
-      for(double i=0; i<Gim30->get_steps; i++){
-	if(Gim30->LRFdata.ranges[i] == 0){
-	  GIM3Ddata.points[i].x = 0;
-	  GIM3Ddata.points[i].y = 0;
-	  GIM3Ddata.points[i].z = 0;
-	  GIM3Ddata.channels[0].values[i] = 0;
-	}
-	else {
-	  tmp = Gim30->GIMangle_new-Gim30->GIMangle_old;
-	  if(tmp > 180)
-	    tmp = 360 - tmp;
-	  tmp = (LRF_END_DEG-LRF_START_DEG)*tmp*i/(360.0*Gim30->get_steps) + Gim30->GIMangle_old + (180.0+LRF_START_DEG)/360.0*tmp;
-	  alpha = atan(0.57734*sin(tmp*M_PI/180.0));
-	  beta = -atan(0.57734*cos(tmp*M_PI/180.0)*cos(alpha));
-	  tmp = (Gim30->LRFdata.angle_max - Gim30->LRFdata.angle_min)*i/Gim30->get_steps + Gim30->LRFdata.angle_min;    
-	  GIM3Ddata.points[i].x = Gim30->LRFdata.ranges[i]*cos(tmp)*cos(beta) + 0.039*sin(beta);
-	  GIM3Ddata.points[i].y = Gim30->LRFdata.ranges[i]*cos(tmp)*sin(alpha)*sin(beta) + Gim30->LRFdata.ranges[i]*sin(tmp)*cos(alpha) - 0.039*sin(alpha)*cos(beta);
-	  GIM3Ddata.points[i].z = -Gim30->LRFdata.ranges[i]*cos(tmp)*cos(alpha)*sin(beta) + Gim30->LRFdata.ranges[i]*sin(tmp)*sin(alpha) + 0.039*cos(alpha)*cos(beta);	}
-      }
-
-      break;
-    }
-
-    GIM3Ddata.header.stamp = ros::Time::now();
-
-
-    sensor_msgs::convertPointCloudToPointCloud2(GIM3Ddata, GIM3Ddata_pc2);
-
-    cloud_pub.publish(GIM3Ddata);
-    cloud_pub_pc2.publish(GIM3Ddata_pc2);
-    r.sleep();
-  }
+    string portname;
+    int fd;
+    struct termios oldtio;
+    struct termios newtio;
+    string data;
+    float GIMangle_old;
+    float GIMangle_new;
   
-  delete Gim30;
+    ros::NodeHandle n;
+    string diag_sub_name;
+  };
 
+  class Get3d : public nodelet::Nodelet, public Gim30::Control
+  {
+  public:
+    Get3d() :
+      pc_pub_name("gim30/point_cloud"),
+      pc2_pub_name("gim30/point_cloud2"),
+      laser_sub_name("gim30/laser"),
+      frame_id("gim30")
+    {}
+  private:
+    virtual void onInit();
+    void callback(const sensor_msgs::LaserScan::ConstPtr& laser);
+  
+    string pc_pub_name;
+    string pc2_pub_name;
+    string laser_sub_name;
+    string frame_id;
+
+    ros::Publisher pc_pub;
+    ros::Publisher pc2_pub;
+    ros::Subscriber sub;
+  };
+}
+
+Gim30::Control::Control() :
+  portname("/dev/ttyUSB0"),
+  diag_sub_name("diagnostic"),
+  fd(-1),
+  old_angle(0.0),
+  new_angle(1.0)
+{
+  n.param("gim30/portname", portname, portname);
+  n.param("gim30/fd", fd, fd);
+  data.resize(8,'\0');
+  SetGim30();
+  StartGim30();
+  
+  diag_msg_sub = n.subscribe<diagnostic_msgs::DiagnosticStatus>(diag_sub_name, 40, &Gim30::Control::callback, this);
+}
+
+Gim30::Control::~Control()
+{
+  StopGim30();
+  CloseGim30();
+}
+
+void Gim30::Control::SetGim30()
+{
+  if(fd > 0){
+    ROS_FATAL("Gim30 is already open");
+  }
+  else if((fd = open(portname.c_str(), O_RDWR) ) < 0){
+    ROS_FATAL("Failed to open the port : Gime30");
+    ROS_BREAK();
+  } 
+  
+  //Get old termios for Gim30
+  if(ioctl(fd, TCGETS, &oldtio) < 0){
+    ROS_FATAL("Gim30 ioctl TCGET error");
+    ROS_BREAK();
+  }
+  //Copy old to new 
+  newtio = oldtio;  
+
+  //New serial settings of Gim30
+  newtio.c_cflag = (B115200 | CS8 | CREAD | CLOCAL);
+  newtio.c_iflag = (IGNPAR | ICRNL);
+  newtio.c_oflag = 0;
+  newtio.c_lflag = ~ICANON;
+
+  //Set new serial settings of Gim30
+  if(ioctl(fd, TCSETS, &newtio) < 0){
+    ROS_FATAL("Gim30 ioctl TCSETS error");
+    ROS_BREAK();
+  }
+  ROS_INFO("Succeed to set Gim30.");
+}
+
+void Gim30::Control::CloseGim30()
+{
+  if(fd < 0){
+    ROS_FATAL("Gim30 Port is already closed.");
+    ROS_BREAK();
+  }
+  close(fd);
+  fd = -1;
+  ROS_INFO("Succeed to close Gim30.");
+}
+
+void Gim30::Control::StartGim30()
+{
+  if(write(fd, "START\r\n", sizeof("START\r\n")) != sizeof("START\r\n")){
+    ROS_FATAL("Gim30 write error. Cannot start Gim30.");
+    ROS_BREAK();
+  }
+  ROS_INFO("Succeed to start GIm30.");
+}
+
+void Gim30::Control::StopGim30()
+{
+  if(write(fd, "STOP\r\n", sizeof("STOP\r\n")) != sizeof("STOP\r\n")){
+    ROS_FATAL("Gim30 write error. Cannot stop Gim30.");
+    ROS_BREAK();
+  }
+  ROS_INFO("Succeed to stop Gim30.");
+}
+
+void Gim30::Control::callback(const diagnostic_msgs::DiagnosticStatus::ConstPtr& diag_msg)
+{
+  old_angle = new_angle;
+  int tmp;
+  for(int i=0; i<8; i+=tmp)
+    tmp = read(fd, &data[i], 8);
+  new_angle = -(atoi(&data[1]) / 100.0);
+}
+
+void Gim30::Get3d::onInit()
+{
+  ros::NodeHandle& private_nh = getPrivateNodeHandle();
+  private_nh.param("gim30/pc_pub_name", pc_pub_name, pc_pub_name);
+  private_nh.param("gim30/pc2_pub_name", pc2_pub_name, pc2_pub_name);
+  private_nh.param("gim30/laser_sub_name", laser_sub_name, laser_sub_name);
+  private_nh.param("gim30/frame_id", frame_id, frame_id);
+
+  
+  pc_pub = private_nh.advertise<sensor_msgs::PointCloud>(pc_pub_name, 40);
+  pc2_pub = private_nh.advertise<sensor_msgs::PointCloud2>(pc2_pub_name, 40);
+  sub = private_nh.subscribe(laser_sub_name, 40, &Get3d::callback, this);
+}
+
+void Gim30::Get3d::callback(const sensor_msgs::LaserScan::ConstPtr& laser)
+{
+  ros::sensor_msgs::PointCloud pc_data;
+  ros::sensor_msgs::PointCloud2 pc2_data;
+
+  pc_data.header.frame_id = frame_id;
+  pc_data.channels.resize(1);
+  pc_data.channels[0].name = "intensities";
+  pc_data.points.resize(laser->ranges.size());
+  pc_data.channels[0].values.resize(laser->intensities.size());
+
+}
+
+int main(int argc, char* argv[])
+{
+  return 0;
 }
